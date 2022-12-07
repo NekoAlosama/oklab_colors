c6dca699f6a446f34acce652b2ce438bb67498e8
Neko Alosama <47763383+NekoAlosama@users.noreply.github.com>
2022-12-06 22:20:23 -0500

    print for second color

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use oklab::*;
 use rayon::prelude::*;
 use rgb::*;
 
-const COMP: f64 = 0.55;
+const COMP: f64 = std::f64::consts::PI / 6.0;
 fn main() {
     // Time for benchmarking purposes
     let start_time = std::time::SystemTime::now();
@@ -13,13 +13,25 @@ fn main() {
 
     // List of all colors to store, including a starting color (might be your background)
     let starting_color = SRgb { r: 0, g: 0, b: 0 };
-    let mut saved_srgb: Vec<SRgb> = vec![starting_color, SRgb { r: 255, g: 255, b: 0 }];
+    let mut saved_srgb: Vec<SRgb> = vec![
+        starting_color,
+        SRgb {
+            r: 255,
+            g: 255,
+            b: 0,
+        },
+    ];
 
     println!("saved_srgb: {saved_srgb:?}");
-    println!("1: {:?} // 0.6: {:?}", saved_srgb[1], Oklab {
-        l: 0.6 * saved_srgb[1].srgb_to_oklab().l,
-        ..saved_srgb[1].srgb_to_oklab()
-    }.oklab_to_srgb_closest());
+
+    // Calculate l mean
+    let l_deltas: Vec<f64> = AllSRgb::default()
+        .par_bridge()
+        .map(|color| color.srgb_to_oklab().l)
+        .collect();
+    let l_mean = l_deltas.par_iter().sum::<f64>() / l_deltas.len() as f64;
+
+    println!("l_mean: {l_mean:?}");
 
     // Calculate HyAB mean for starting color
     let starting_color_deltas: Vec<f64> = AllSRgb::default()
@@ -32,16 +44,37 @@ fn main() {
         .collect();
     let starting_color_mean =
         starting_color_deltas.par_iter().sum::<f64>() / starting_color_deltas.len() as f64;
-    
+
     println!("starting_color_mean: {starting_color_mean:?}");
 
-    // Create an iterator that pre-filters out colors based on starting_color_mean
-    let filtered_values = AllSRgb::default().par_bridge().filter(|color| {
-        color
+    // Print second color
+    println!(
+        "1: S{:?} // 0.6: S{:?} // DE: {:?} // l: {:?}",
+        saved_srgb[1],
+        Oklab {
+            l: 0.6 * saved_srgb[1].srgb_to_oklab().l,
+            ..saved_srgb[1].srgb_to_oklab()
+        }
+        .oklab_to_srgb_closest(),
+        saved_srgb[1]
             .srgb_to_oklab()
-            .delta_hyab(starting_color.srgb_to_oklab())
-            > starting_color_mean
-    });
+            .delta_hyab(starting_color.srgb_to_oklab()),
+        saved_srgb[1].srgb_to_oklab().l
+    );
+
+    // Create an iterator that pre-filters out colors based on starting_color_mean
+    let filtered_values = AllSRgb::default()
+        .par_bridge()
+        .filter(|color| {
+            color.srgb_to_oklab().l > starting_color.srgb_to_oklab().l.max(l_mean)
+                || color.srgb_to_oklab().l < starting_color.srgb_to_oklab().l.min(l_mean)
+        })
+        .filter(|color| {
+            color
+                .srgb_to_oklab()
+                .delta_hyab(starting_color.srgb_to_oklab())
+                > starting_color_mean
+        });
 
     // Find 9 new colors
     // This produces a list of 8 well-contrasting colors and one excess color
@@ -173,25 +206,17 @@ Minimum mean color difference (estimate):
 
 Median doesn't seem good since it might return as 0.0 for many colors
 
-COMP: 0.55
+COMP: 0.5
 saved_srgb: [Rgb { r: 0, g: 0, b: 0 }, Rgb { r: 255, g: 255, b: 0 }]
-starting_color_mean: 0.7879053739478907
-2: SRgb { r: 174, g: 0, b: 255 } // 0.6: SRgb { r: 80, g: 0, b: 136 } // DE: 0.8856062180812202 // l: 0.5864670698235025
-3: SRgb { r: 0, g: 199, b: 253 } // 0.6: SRgb { r: 0, g: 95, b: 143 } // DE: 0.5014721255353223 // l: 0.7729993400576811
-4: SRgb { r: 255, g: 85, b: 0 } // 0.6: SRgb { r: 142, g: 0, b: 0 } // DE: 0.4614032257831019 // l: 0.6758924968256405
-5: SRgb { r: 0, g: 152, b: 0 } // 0.6: SRgb { r: 0, g: 74, b: 0 } // DE: 0.4157467762816877 // l: 0.5889019305640453
-6: SRgb { r: 255, g: 208, b: 237 } // 0.6: SRgb { r: 138, g: 97, b: 123 } // DE: 0.317962646542098 // l: 0.9064095933177545
-7: SRgb { r: 0, g: 77, b: 255 } // 0.6: SRgb { r: 0, g: 0, b: 156 } // DE: 0.2852811203526471 // l: 0.5201799787357348
-8: SRgb { r: 249, g: 178, b: 42 } // 0.6: SRgb { r: 131, g: 84, b: 0 } // DE: 0.2714124119654985 // l: 0.8105051200012297
-
-COMP: 0.55
-saved_srgb: [Rgb { r: 0, g: 0, b: 0 }, Rgb { r: 255, g: 255, b: 255 }]
+l_mean: 0.6374367230074206
 starting_color_mean: 0.7879053739478898
-2: SRgb { r: 98, g: 0, b: 255 } // 0.6: SRgb { r: 32, g: 0, b: 137 } // DE: 0.7939227742807733 // l: 0.5001367631674302
-3: SRgb { r: 0, g: 162, b: 0 } // 0.6: SRgb { r: 0, g: 79, b: 0 } // DE: 0.5928757036749703 // l: 0.6171115421336347
-4: SRgb { r: 255, g: 91, b: 175 } // 0.6: SRgb { r: 142, g: 0, b: 93 } // DE: 0.5025946985016285 // l: 0.7109459244275075
-5: SRgb { r: 0, g: 197, b: 255 } // 0.6: SRgb { r: 0, g: 94, b: 144 } // DE: 0.3810114118348807 // l: 0.7692645746107217
-6: SRgb { r: 255, g: 175, b: 113 } // 0.6: SRgb { r: 145, g: 76, b: 0 } // DE: 0.3038395393252375 // l: 0.8183737595630627
-7: SRgb { r: 219, g: 0, b: 32 } // 0.6: SRgb { r: 110, g: 0, b: 0 } // DE: 0.27742733304017564 // l: 0.5612704708488127
-8: SRgb { r: 198, g: 0, b: 249 } // 0.6: SRgb { r: 93, g: 0, b: 134 } // DE: 0.2764380413357115 // l: 0.6145173008284384
+1: SRgb { r: 255, g: 255, b: 0 } // 0.6: SRgb { r: 111, g: 134, b: 0 } // DE: 1.178988628052311 // l: 0.9679827203267873
+2: SRgb { r: 211, g: 2, b: 255 } // 0.6: SRgb { r: 99, g: 0, b: 139 } // DE: 0.8344607206863967 // l: 0.6375615559609987
+3: SRgb { r: 0, g: 202, b: 254 } // 0.6: SRgb { r: 0, g: 97, b: 142 } // DE: 0.49213886263453943 // l: 0.7804486680945886
+4: SRgb { r: 255, g: 108, b: 0 } // 0.6: SRgb { r: 146, g: 18, b: 0 } // DE: 0.424408051787352 // l: 0.7035513718039714
+5: SRgb { r: 0, g: 170, b: 0 } // 0.6: SRgb { r: 0, g: 83, b: 0 } // DE: 0.3770062120030873 // l: 0.6394486105627715
+6: SRgb { r: 255, g: 208, b: 231 } // 0.6: SRgb { r: 138, g: 97, b: 118 } // DE: 0.31135197796903935 // l: 0.9045818012261558
+7: SRgb { r: 66, g: 133, b: 255 } // 0.6: SRgb { r: 0, g: 50, b: 166 } // DE: 0.25783561185514864 // l: 0.6377244660630951
+8: SRgb { r: 8, g: 253, b: 203 } // 0.6: SRgb { r: 0, g: 129, b: 79 } // DE: 0.2505776470783697 // l: 0.8854319415597356
+
 */
