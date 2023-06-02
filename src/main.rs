3d72be20fdc74ec0a0b061efefe3e93bd385842d
Neko Alosama <47763383+NekoAlosama@users.noreply.github.com>
2023-06-02 18:27:34 -0400

    Arithmetic mean for color_center

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,15 @@ fn main() {
 
     let mut saved_colors = vec![SRgb { r: 0, g: 0, b: 0 }];
 
-    let color_center = (SRgb::all_colors()
+    let color_center = SRgb::all_colors()
         .par_bridge()
-        .filter(|sample_color| *sample_color != saved_colors[0])
         .map(|sample_color| {
             saved_colors[0]
                 .srgb_to_oklab()
                 .delta_e(sample_color.srgb_to_oklab())
         })
-        .map(|delta| delta.ln_1p())
         .sum::<f64>()
-        / (2.0_f64.powi(24) - 1.0))
-        .exp_m1();
+        / 2.0_f64.powi(24);
 
     println!("color_center: {color_center:.5}");
 
@@ -44,24 +41,6 @@ fn main() {
                     .delta_e(saved_colors[0].srgb_to_oklab())
                     > color_center
             })
-            /*
-            // Hue filter, don't know if this is best
-            .filter(|test_srgb| {
-                let test_color = test_srgb.srgb_to_oklab();
-                let hue_differences = starting_colors
-                    .clone()
-                    // Check if the chorma of the starting color is greater than 0.0
-                    // In .delta_h_relative(), not checking would result in a divide-by-zero
-                    .filter(|color| (color.a.powi(2) + color.b.powi(2)) > 0.0)
-                    .map(|color| color.delta_h_relative(test_color));
-                let minimum = hue_differences.fold(f64::MAX, |a, b| if a < b { a } else { b });
-                if minimum < (1.0 / 3.0_f64) {
-                    false
-                } else {
-                    true
-                }
-            })
-            */
             .for_each(|test_srgb| {
                 let test_colors = starting_colors
                     .clone()
@@ -72,8 +51,8 @@ fn main() {
                     .map(|vector| vector[0].delta_e(vector[1]));
 
                 let all_deltas_count = all_deltas.clone().count() as f64;
-                let delta = (all_deltas.map(|delta| delta.ln_1p()).sum::<f64>() / all_deltas_count)
-                    .exp_m1();
+                let delta =
+                    (all_deltas.map(|delta| delta.ln()).sum::<f64>() / all_deltas_count).exp();
 
                 let mut locked_saved_delta = saved_delta.lock();
                 let mut locked_saved_color = saved_color.lock();
@@ -97,36 +76,35 @@ fn main() {
             std::process::exit(99);
         } else {
             println!(
-                "{saved_color}, {}, {b:.5}",
+                "{saved_color}, {}, {:.5?}",
                 Oklab {
                     l: (saved_color.srgb_to_oklab().l * 2.0 / 3.0),
                     a: saved_color.srgb_to_oklab().a / 3.0,
                     b: saved_color.srgb_to_oklab().b / 3.0
                 }
                 .oklab_to_srgb_closest(),
-                b = saved_delta.into_inner()
+                saved_delta.into_inner()
             );
             saved_colors.push(saved_color)
         }
     }
 
     println!(
-        "Total time: {:?}",
+        "Total time: {:.3?}",
         start_time.elapsed().expect("Time went backwards")
     );
 }
 
 /*
-delta_e_eok
-geometric mean and no hue filter
-color_center: 0.65097
+arithmetic mean for color_center
+color_center: 0.65910
 (255, 255, 255), (148, 148, 148), 1.00000
-(255, 0, 255), (122, 66, 120), 0.72148
-(0, 255, 0), (86, 136, 82), 0.66345
-(168, 0, 255), (81, 51, 109), 0.58931
-(255, 255, 0), (144, 146, 95), 0.55638
-(242, 0, 0), (110, 53, 45), 0.52851
-(0, 162, 0), (51, 84, 49), 0.50720
-(0, 129, 255), (48, 76, 112), 0.48904
-Total time: 241.7516222s
+(255, 0, 255), (122, 66, 120), 0.69744
+(0, 255, 0), (86, 136, 82), 0.63267
+(246, 0, 0), (112, 54, 46), 0.56269
+(0, 134, 255), (50, 78, 113), 0.52354
+(255, 255, 0), (144, 146, 95), 0.49229
+(175, 0, 255), (85, 52, 110), 0.47025
+(0, 165, 0), (52, 86, 50), 0.45863
+Total time: 225.157s
 */
